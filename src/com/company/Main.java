6e0b51708d41bbc9b71a84587a27219fa1a7de40
frolovitsme <frolovitsme@gmail.com>
2021-12-27 00:03:05 +0300

    fake commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ import com.company.utils.GameFrame;
 
 public class Main {
     public static void main(String[] args) {
-        GameFrame frame = new GameFrame();
+        GameFrame gameFrame = new GameFrame();
     }
 }
